839971aa2b836ff2607b8d8bcdf36b010365f370
JAMATX38 <JAIMEMTX38@GMAIL.COM>
2021-11-16 22:04:10 -0600

    worked on server.js, routes files

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,8 +1,35 @@
 const router = require("express").Router();
+const { Workout }= require("../../models");
+
+router.get("/", async (req, res) => {
+    const exercises = await Workout.find({})
+    res.json(exercises)
+})
+
+router.put("/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body }},
+        { new:true }
+    )
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+    Workout.create({})
+    .then((data) => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
 
-router.get('/',(req,res) => {
-    res.send('Hello World')
-}) 
 
 module.exports = router; 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
+const homeyRoutes = require("./homeyRoutes");
+const Workout = require("../models/Workout");
 const apiRoutes = require('./api');
 
 
-
+router.use('/', homeyRoutes);
 router.use('/api', apiRoutes);
+
 
 module.exports = router;
