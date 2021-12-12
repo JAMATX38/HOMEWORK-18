8e6b48c78098c7fd847a8567e945a6e386ce5645
JAMATX38 <JAIMEMTX38@GMAIL.COM>
2021-12-12 14:47:03 -0600

    added syntax for MongoAtlas

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(routes);
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
