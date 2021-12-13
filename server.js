9a90313aac69574477b4440e514e5415401de28d
JAMATX38 <JAIMEMTX38@GMAIL.COM>
2021-12-13 16:57:00 -0600

    fevn

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3000
+const path = require("path");
 
 const Workout = require('./models/Workout');
 const app = express();
